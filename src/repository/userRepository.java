25d447a23454ee42f6d2e0bb6bc11f498d13fede
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2026-05-04 10:41:38 -0300

    Class UsuarioRepository

--- src/repository/userRepository.java
+++ src/repository/userRepository.java
@@ -1,4 +1,29 @@
 package repository;
 
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class userRepository {
+
+    private List<User> usuarios = new ArrayList<>();
+
+    public void salvar(User user){
+        usuarios.add(user);
+    }
+
+    public List<User> listaUsers(){
+        return usuarios;
+    }
+
+    public User buscarId(Long id){
+        return usuarios.stream()
+                .filter(u -> u.getId()
+                .equals(id)).findFirst().orElse(null);
+    }
+
+    public void deletar(Long id){
+        usuarios.removeIf(u -> u.getId().equals(id));
+    }
 }
