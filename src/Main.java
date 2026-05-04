817b37119655a8335d754bc67a744e2e617e7946
pyetro-cod <pyetro.f@aluno.uepb.edu.br>
2026-05-04 11:02:52 -0300

    Class main loopTest

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,40 @@
+import service.UserService;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    Scanner sc = new Scanner(System.in);
+    UserService service = new UserService();
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+    int opc;
+
+    System.out.println("\n=== CRUD DE USUÁRIOS ===");
+    System.out.println("1 - Criar usuário");
+    System.out.println("2 - Listar usuários");
+    System.out.println("3 - Atualizar usuário");
+    System.out.println("4 - Deletar usuário");
+    System.out.println("0 - Sair");
+    System.out.print("Escolha: ");
+
+    opc = sc.nextInt();
+    sc.nextLine();
+    switch (opc) {
+        case 1:
+            System.out.println("ID: ");
+            Long id = sc.nextLong();
+            sc.nextLine();
+
+            System.out.println("Nome: ");
+            String nome = sc.nextLine();
+
+            System.out.println("Email: ");
+            String email = sc.nextLine();
+
+            service.criar(id,nome,email);
+            System.out.println("Usuário Criado com Sucesso");
+            break;
+
+
     }
 }
